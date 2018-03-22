603c56e06c6d0cb04371160f88283a9fbf781d68
Josip Maric <joza.m44@gmail.com>
2018-03-22 17:09:54 +0100

    Pixel Art Application and h1 and h2 color change.

--- designs.js
+++ designs.js
@@ -1,27 +1,33 @@
 
-// Select color input
-$('#colorPicker').change(function(){
-  var setColor=$('#colorPicker').val();
-})
-// Select size input
-var  h=$('#inputHeight').val();
-var  w=$('#inputWeight').val();
-// When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function(event){
-  makeGrid();
-});
-//seting id of cells
-var boxId=0;
-//creating grid
-function makeGrid(){
-  var grid='';
-for(i=0;i<h;i++){
-  var grid+='<tr> ';
-  for(n=0;n<w;n++){
-    var grid+='<td>'+boxId+'</td>';
-    boxId++;
+var h,w,color;
+$(document).ready(function(){
+	$('#sizePicker').submit(function(event){
+		event.preventDefault();
+		// Select size input
+		h=$('#inputHeight').val();
+		w=$('#inputWeight').val();
+		makeGrid(h,w);
+  });
+	// When size is submitted by the user, call makeGrid()
+		function makeGrid(x,y){
+			$('tr').remove();//reseting grid on default
+			for(var i=1;i<=h;i++){
+		$("#pixelCanvas").append('<tr id=idbox'+i+'></tr>');
+				for(var n=1;n<=w;n++){
+				$('#idbox'+i).append('<td></td>');
+			}
+		}
+
+	// on click check if box has a style if it has remove color else add color
+		$('td').click(function addColor(){
+			// Select color input
+			 color = $('#colorPicker').val();
+		if($(this).attr('style')){
+			$(this).removeAttr('style');
+		}
+		else{
+			$(this).css('background-color',color);
+		}
+		});
   }
-var grid+=' </tr>';
-}
-grid.append($('#pixelCanvas'));
-};
+});
